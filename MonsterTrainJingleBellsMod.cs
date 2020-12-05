92017c24614b1a9690c7fccfeee9b0e61401e346
Chris Dietsch <chdietsc@microsoft.com>
2020-12-05 15:11:50 -0500

    remove Frank testing

--- MonsterTrainJingleBellsMod.cs
+++ MonsterTrainJingleBellsMod.cs
@@ -18,30 +18,4 @@ namespace MonsterTrainJingleBellsMod
             harmony.PatchAll();
         }
     }
-
-    [HarmonyPatch(typeof(CharacterData))]
-    [HarmonyPatch("GetName")]
-    public static class NameChangerFrank
-    {
-        private static void Postfix(ref string __result)
-        {
-            if (__result == "Hornbreaker Prince")
-            {
-                __result = "Frank";
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(CardState))]
-    [HarmonyPatch("GetTitle")]
-    public static class NameChangerFrankCard
-    {
-        private static void Postfix(ref string __result)
-        {
-            if (__result == "Hornbreaker Prince")
-            {
-                __result = "Frank";
-            }
-        }
-    }
 }
